d70950724d38654f64b0afa154da9f06136c17bb
Ashu Choudhary <thisisashuchoudhary@gmail.com>
2025-07-22 16:44:20 +0530

    fix: update setup.py for proper CARE plugin installation

--- setup.py
+++ setup.py
@@ -2,51 +2,12 @@
 
 """The setup script."""
 
-from setuptools import find_packages, setup
-
-with open("README.md") as readme_file:
-    readme = readme_file.read()
-
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
-requirements = [
-    "requests",
-    "celery",
-    "django",
-    "djangorestframework",
-    "django-environ",
-    "django-filter",
-    "jsonschema",
-]
-
-test_requirements = []
+from setuptools import setup, find_packages
 
 setup(
-    author="Open Healthcare Network",
-    author_email="info@ohc.network",
-    python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    description="Care WhatsApp Bot Plugin for CARE",
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + "\n\n" + history,
-    include_package_data=True,
-    keywords="care_whatsapp_bot",
     name="care_whatsapp_bot",
-    packages=find_packages(include=["care_whatsapp_bot", "care_whatsapp_bot.*"]),
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/ohcnetwork/care_hello",
-    version="0.2.0",
-    zip_safe=False,
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
 )
