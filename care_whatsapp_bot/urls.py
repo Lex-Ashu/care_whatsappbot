8375cb5bea1cc80514899360ddf38514afedbfe4
Ashu Choudhary <thisisashuchoudhary@gmail.com>
2025-07-29 00:42:11 +0530

    Fix urls.py

--- care_whatsapp_bot/urls.py
+++ care_whatsapp_bot/urls.py
@@ -1,13 +1,7 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-from care_whatsapp_bot.api.viewsets.whatsapp import CareWhatsappBotViewset
 from care_whatsapp_bot.api.viewsets.whatsapp import WhatsAppWebhookView
 
-router = DefaultRouter()
-
-router.register("whatsapp", CareWhatsappBotViewset, basename="care_whatsapp_bot__whatsapp")
-
-urlpatterns = router.urls + [
+urlpatterns = [
     path('webhook/', WhatsAppWebhookView.as_view(), name='whatsapp-webhook'),
 ]
